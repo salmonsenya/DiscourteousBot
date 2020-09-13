950389f0d89a62a1366c1a548e68be8367f1ef86
Kseniia Khromova <kseniiakhromova@yandex.ru>
2020-09-14 01:48:52 +0300

    bot prototype

---
+++ DiscourteousBot/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+
+namespace DiscourteousBot
+{
+    class Program
+    {
+        static ITelegramBotClient botClient;
+        static void Main(string[] args)
+        {
+            botClient = new TelegramBotClient("");
+            var me = botClient.GetMeAsync().Result;
+            Console.WriteLine($"Hello! DiscourteousBot is started.");
+
+            botClient.OnMessage += Bot_OnMessage;
+            botClient.StartReceiving();
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+
+            botClient.StopReceiving();
+        }
+
+        static async void Bot_OnMessage(object sender, MessageEventArgs e)
+        {
+            var input = e.Message.Text;
+            if (input != null)
+            {
+                var regexYes = new Regex(@"(^((да)|(Да)|(ДА))(\?*|\.*|!*)$)|(((\s|,)да\?+)$)");
+                if (regexYes.IsMatch(input))
+                {
+                    await botClient.SendTextMessageAsync(
+                        chatId: e.Message.Chat.Id,
+                        replyToMessageId: e.Message.MessageId,
+                        text: $"пизда");
+                }
+
+                var regexNo = new Regex(@"(^((нет)|(Нет)|(НЕТ))(\.*|!*)$)");
+                if (regexNo.IsMatch(input))
+                {
+                    await botClient.SendTextMessageAsync(
+                        chatId: e.Message.Chat.Id,
+                        replyToMessageId: e.Message.MessageId,
+                        text: $"пидора ответ");
+                }
+            }
+        }
+    }
+}
